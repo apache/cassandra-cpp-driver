e6426dbd62ff0785f1b4881a60cee1e31dc94897
absurdfarce <bret.mcguire@ibm.com>
2026-03-12 14:29:25 -0500

    CASSCPP-3 Crash on Pure virtual function while the destructor of the Session object is called
    patch by Bret McGuire; reviewed by Bret McGuire and Yifan Cai

--- src/connection.hpp
+++ src/connection.hpp
@@ -87,7 +87,7 @@ public:
    *
    * @param connection The closing connection.
    */
-  virtual void on_close(Connection* connection) = 0;
+  virtual void on_close(Connection* connection) {}
 };
 
 /**
